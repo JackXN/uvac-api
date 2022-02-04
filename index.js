85653a57ade43d23c5425ba8e0ef7548bf787cda
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2022-02-04 13:59:52 -0700

    initial commit and file setup

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+dotenv.config();
+
+
+const PORT = process.env.PORT;
+
+
+
+mongoose.connect(process.env.DB_URL).then(() => {
+    console.log('Access to database granted....')
+})
+.catch((error) => {
+    console.log(error)
+}) 
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.listen(process.env.PORT || 5000,  () => console.log(`UVAC server is running on PORT ${PORT}`))
