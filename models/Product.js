43d44d22da372a69ad5ca37756adc0d4423cb0e1
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2022-03-01 17:59:45 -0700

    created product schema

--- models/Product.js
+++ models/Product.js
@@ -2,6 +2,19 @@ const mongoose = require('mongoose');
 
 const ProductSchema = new mongoose.Schema(
     {
-        
+      productID: {type: Number, required: true},
+        producName: {type: String, required: true},
+        productDescription: {type: String, required: true},
+        productCategory: {type: String, required: true},
+        inStock: {type: Boolean, required: true},
+        onSale: {type: Boolean, required: false},
+        Type: {type: String, required: true},
+        Brand: {type:String, required: true},
+        itemDimensions: {type: String, required: false},
+
+
     }
 )
+
+
+module.exports = mongoose.model('Product', ProductSchema)
