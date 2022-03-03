4c20f9672457404025ca25bc6da628bbe37ea93b
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2022-03-02 20:07:44 -0700

    GET, PUT DELETE  product endpoints

--- routes/product.js
+++ routes/product.js
@@ -24,5 +24,47 @@ router.get('/', async(req,res) => {
 })
 
 
+// Get a single product
+
+router.get('/find/:id', async (req,res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        res.status(200).json(product)
+    }catch(error) {
+        console.log(error)
+    }
+})
+
+
+// Delete a product
+router.delete('/:id', async (req,res) => {
+    try {
+        await Product.findByIdAndDelete(req.params.id);
+        res.status(200).json('Product Has Been Deleted')
+    }
+    catch(error) {
+        console.log(error)
+    }
+});
+
+// Update A Product
+router.put('/:id', async(req,res) => {
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(req.params.id,
+            
+            {
+                $set: req.body,
+            },
+            {new: true}
+            
+            );
+            res.status(200).json(updatedProduct);
+
+    }catch(error) {
+        console.log(error)
+    }
+})
+
+
 
 module.exports = router;
