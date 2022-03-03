ad59902bb860126ff3ff28e692948261c6f9d134
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2022-03-02 19:51:23 -0700

    typo fixes

--- models/Product.js
+++ models/Product.js
@@ -3,14 +3,14 @@ const mongoose = require('mongoose');
 const ProductSchema = new mongoose.Schema(
     {
       productID: {type: Number, required: true},
-        producName: {type: String, required: true},
+        productName: {type: String, required: true},
         productDescription: {type: String, required: true},
         productCategory: {type: String, required: true},
         inStock: {type: Boolean, required: true},
         onSale: {type: Boolean, required: false},
         salePrice: {type: Number, required: false},
-        Type: {type: String, required: true},
-        Brand: {type:String, required: false},
+        type: {type: String, required: true},
+        brand: {type:String, required: false},
         itemDimensions: {type: String, required: false},
         color: {type: String, required: false}
 
